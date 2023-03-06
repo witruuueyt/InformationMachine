ce90e87f623b1b062e776e4b24fd6263c877011b
witruuueyt <926357@swansea.ac.uk>
2023-03-06 16:53:02 +0000

    cq3g5gx3qgqgxwaef234

--- Assets/Scripts/CheckConnection.cs
+++ Assets/Scripts/CheckConnection.cs
@@ -4,25 +4,29 @@ using TMPro;
 
 
 
-public class ExampleFunctions : MonoBehaviour
+public class CheckConnection : MonoBehaviour
 {
     [Header("Factory Interfaces")]
     public OPCUA_Interface[] opcuaInterfaces;
     public TMP_Text feedbackText;
-
+    string string1 = "";
+    
     //runs through all of the factory interfaces checking for connection
     public void CheckConnectionWithMachines()
     {
+        feedbackText.SetText(string1); //rewrite message to nothing
+
         for (int i = 0; i < opcuaInterfaces.Length; i++)             
         {
+            
             if (opcuaInterfaces[i].IsConnected)               
             {
-                feedbackText.text = feedbackText.text + "Good connection to interface " + i + ".";
+                feedbackText.text = feedbackText.text + "Good connection to interface " + i + "." + "\r\n"; //println
                 Debug.Log("Good connection to interface " + i + ".");
             }
             else                                     
             {
-                feedbackText.text = feedbackText.text + "Good connection to interface " + i + ".";
+                feedbackText.text = feedbackText.text + "No connection to interface " + i + "." + "\r\n"; //println
                 Debug.Log("No connection to interface " + i + ".");
             }
         }
